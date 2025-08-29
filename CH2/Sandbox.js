ffe73883dff807887f1b460612b5a17fc5fc37a9
Jbijeesh <jbijeesh@rrc.ca>
2025-08-29 09:52:49 -0500

    chapter 2_srtings

--- CH2/Sandbox.js
+++ CH2/Sandbox.js
@@ -1,3 +1,5 @@
-let age = 25;
-let year = 2019;
-console.log(age,year);
+let first_name  = 'Jithu';
+let last_name  = 'Bijeesh';
+let full_name  = first_name + ' ' + last_name;
+
+console.log(full_name);
