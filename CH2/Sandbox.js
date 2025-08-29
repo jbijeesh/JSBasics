332dbb04dab414e5d2b147e4c88bc2f47e232a70
Jbijeesh <jbijeesh@rrc.ca>
2025-08-29 09:54:43 -0500

    chapter 2_srtings_1.2

--- CH2/Sandbox.js
+++ CH2/Sandbox.js
@@ -3,3 +3,8 @@ let last_name  = 'Bijeesh';
 let full_name  = first_name + ' ' + last_name;
 
 console.log(full_name);
+console.log(full_name[2]);
+console.log(full_name.toUpperCase());
+console.log(full_name.length);
+
+
