00da66dc5e2b47ecb3ee6a5d5d8f4f0bfb1d0378
Jbijeesh <jbijeesh@rrc.ca>
2025-09-01 10:20:44 -0500

    if

--- CH3/Sandbox.js
+++ CH3/Sandbox.js
@@ -1,37 +1,17 @@
-// switch statements
-const grade = 'D';
+// break & continue
+const scores = [50, 25, 0, 30, 100, 20, 10];
 
-switch(grade){
-  case 'A':
-    console.log('you got an A!');
+for(let i = 0; i < scores.length; i++){
+
+  if(scores[i] === 0){
+    continue;
+  }
+
+  console.log('your score:', scores[i]);
+
+  if(scores[i] === 100){
+    console.log('congrats, you got the top score!');
     break;
-  case 'B':
-    console.log('you got a B!');
-    break;
-  case 'C':
-    console.log('you got a C!');
-    break;
-  case 'D':
-    console.log('you got a D!');
-    break;
-  case 'E':
-    console.log('you got an E!');
-    break;
-  default:
-    console.log('not a valid grade');
+  }
+
 }
-
-// using if statements
-// if(grade === 'A'){
-
-// } else if(grade === 'B'){
-
-// } else if(grade === 'C'){
-
-// } else if(grade === 'D'){
-
-// } else if(grade === 'E'){
-
-// } else {
-
-// }
