8a37dc5e8c8772306367b9d76ba86f9f266adff3
Jbijeesh <jbijeesh@rrc.ca>
2025-09-01 10:08:11 -0500

    chapter 2_likes

--- CH2/Sandbox.js
+++ CH2/Sandbox.js
@@ -1,6 +1,23 @@
-let radi = 5;
-const pi = 3.14;
-let area  = pi * radi**2;
-console.log(area);
-let curm  = pi * radi * 2;
-console.log(curm);
+// template strings
+const title = 'Best reads of 2019';
+const author = 'Mario';
+const likes = 30;
+
+// concatenation way
+
+// let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
+// console.log(result);
+
+// template string way
+
+// let result = `The blog called ${title} by ${author} has ${likes} likes`;
+// console.log(result);
+
+// creating html templates
+let html = `
+  <h2>${title}</h2>
+  <p>By ${author}</p>
+  <span>This blog has ${likes} likes</span>
+`;
+
+console.log(html);
