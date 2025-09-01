967269e88905b09a92c8faf247bfda425e1117aa
Jbijeesh <jbijeesh@rrc.ca>
2025-09-01 10:14:25 -0500

    booleans & comparisons_1.2

--- CH2/Sandbox.js
+++ CH2/Sandbox.js
@@ -1,30 +1,15 @@
-// booleans & comparisons
-// console.log(true, false, 'true', 'false');
+// type conversion
+// let score = '100';
 
-// methods can return booleans
-// let email = 'luigi@thenetninja.co.uk';
-// let names = ['mario', 'luigi', 'toad'];
+// score = Number(score);
+// console.log(score + 1);
+// console.log(typeof score);
 
-// let result = email.includes('@');
-// let result = names.includes('luigi');
+// let result = Number('hello');
+// let result = String(50);
+// let result = Boolean(100);
+// let result = Boolean(0);
+// let result = Boolean('0');
+let result = Boolean('');
 
-// console.log(result);
-
-// comparison operators
-let age = 25;
-
-console.log(age == 25);
-console.log(age == 30);
-console.log(age != 30);
-console.log(age > 20);
-console.log(age < 20);
-console.log(age <= 25);
-console.log(age >= 25);
-
-let name = 'shaun';
-
-console.log(name == 'shaun');
-console.log(name == 'Shaun');
-console.log(name > 'crystal');
-console.log(name > 'Shaun');
-console.log(name > 'Crystal');
+console.log(result, typeof result);
