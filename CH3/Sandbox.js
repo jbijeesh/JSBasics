69334d1aa72a12102dbf1574abeead37c9637da6
Jbijeesh <jbijeesh@rrc.ca>
2025-09-01 10:20:25 -0500

    switch

--- CH3/Sandbox.js
+++ CH3/Sandbox.js
@@ -1,7 +1,37 @@
-// do while loops
-let i = 5;
+// switch statements
+const grade = 'D';
 
-do{
-  console.log('val of i is: ', i);
-  i++;
-} while(i < 5);
+switch(grade){
+  case 'A':
+    console.log('you got an A!');
+    break;
+  case 'B':
+    console.log('you got a B!');
+    break;
+  case 'C':
+    console.log('you got a C!');
+    break;
+  case 'D':
+    console.log('you got a D!');
+    break;
+  case 'E':
+    console.log('you got an E!');
+    break;
+  default:
+    console.log('not a valid grade');
+}
+
+// using if statements
+// if(grade === 'A'){
+
+// } else if(grade === 'B'){
+
+// } else if(grade === 'C'){
+
+// } else if(grade === 'D'){
+
+// } else if(grade === 'E'){
+
+// } else {
+
+// }
