f5cb4d1429a4c0d0b7d588ef4a8a270be17be4f2
Jbijeesh <jbijeesh@rrc.ca>
2025-09-01 10:26:10 -0500

    functioncall backs

--- CH4/Sandbox.js
+++ CH4/Sandbox.js
@@ -1,39 +1,12 @@
-// arrow functions
+// callbacks & forEach
+let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
 
-// regular function
-// const calcArea = function(radius){
-//   return 3.14 * radius**2;
-// }
-
-// arrow function
-const calcArea = radius => 3.14 * radius**2;
-
-const area = calcArea(5);
-console.log('area is:', area);
-
-// practise arrow functions
-
-// const greet = function(){
-//   return 'hello, world';
-// }
-
-const greet = () => 'hello, world';
-
-// const bill = function(products, tax){
-//   let total = 0;
-//   for(let i = 0; i < products.length; i++){
-//     total += products[i] + products[i] * tax;
-//   }
-//   return total;
-// }
-
-const bill = (products, tax) => {
-  let total = 0;
-  for(let i = 0; i < products.length; i++){
-    total += products[i]  + products[i] * tax;
-  }
-  return total;
+const logPerson = (person, index) => {
+  console.log(`${index} - hello ${person}`);
 }
 
-console.log(greet());
-console.log(bill([10,15,30], 0.2));
+// people.forEach(person => {
+//   console.log(`hello ${person}`);
+// });
+
+people.forEach(logPerson);
